b846494cc64b73e6869bd6d27bd53c51a5126bfd
zv0a <109912864+zv0a@users.noreply.github.com>
2022-07-30 15:43:48 +0300

    wrote the code

---
+++ CW2/CW2.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CW2 : MonoBehaviour
+{
+    string heroname = "joy";
+        int age = 19;
+    int hight = 170;
+    string superpower = "water & fier";
+    string villanname = "ray";
+        int age = 20;
+    int hight = 177;
+    string villansuperwpower = "light";
+          
+       
+       int age differance = villan age = hero age;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hero name is" + hero name + "and his age is" + hero age);
+        print("villan name is" + hero name + "and his age is" + hero age);
+        print("hero hight is" + hero hight + "and his power is" + superpower);
+        print("villan hight is" + viilan hight + "and his power is" + super power);
+        print("age differance bitween them" + age differance);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+    }
+}
